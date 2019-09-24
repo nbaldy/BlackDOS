c030a6ab64d5bfd4778bbbd1e1c7c00d1b1b17d1
Tim Inzitari <tsi3@zips.uakron.edu>
2019-09-24 09:22:39 -0400

    Set Up for Lab 3
    Set up for Lab 3

--- kernel.c
+++ kernel.c
@@ -29,44 +29,7 @@ void printLogo();
 
 void main()
 {
-  char food[25], adjective[25], color[25], animal[25];
-  int temp;
-  makeInterrupt21();
-  printLogo();
-  interrupt(33,0,"\r\nWelcome to the Mad Libs kernel.\r\n\0",0,0);
-  interrupt(33,0,"Enter a food: \0",0,0);
-  interrupt(33,1,food,0,0);
-  temp = 0;
-  while ((temp < 100) || (temp > 120)) {
-     interrupt(33,0,"Enter a number between 100 and 120: \0",0,0);
-     interrupt(33,14,&temp,0,0);
-  }
-  interrupt(33,0,"Enter an adjective: \0",0,0);
-  interrupt(33,1,adjective,0,0);
-  interrupt(33,0,"Enter a color: \0",0,0);
-  interrupt(33,1,color,0,0);
-  interrupt(33,0,"Enter an animal: \0",0,0);
-  interrupt(33,1,animal,0,0);
-  interrupt(33,0,"Your note is on the printer, go get it.\r\n\0",0,0);
-  interrupt(33,0,"Dear Professor O\'Neil,\r\n\0",1,0);
-  interrupt(33,0,"\r\nI am so sorry that I am unable to turn in my program at this time.\r\n\0",1,0);
-  interrupt(33,0,"First, I ate a rotten \0",1,0);
-  interrupt(33,0,food,1,0);
-  interrupt(33,0,", which made me turn \0",1,0);
-  interrupt(33,0,color,1,0);
-  interrupt(33,0," and extremely ill.\r\n\0",1,0);
-  interrupt(33,0,"I came down with a fever of \0",1,0);
-  interrupt(33,13,temp,1,0);
-  interrupt(33,0,". Next my \0",1,0);
-  interrupt(33,0,adjective,1,0);
-  interrupt(33,0," pet \0",1,0);
-  interrupt(33,0,animal,1,0);
-  interrupt(33,0," must have\r\nsmelled the remains of the \0",1,0);
-  interrupt(33,0,food,1,0);
-  interrupt(33,0," on my computer, because he ate it. I am\r\n\0",1,0);
-  interrupt(33,0,"currently rewriting the program and hope you will accept it late.\r\n\0",1,0);
-  interrupt(33,0,"\r\nSincerely,\r\n\0",1,0);
-  interrupt(33,0,"(Elena, Nicole, and Tim)\r\n\0",1,0);
+  
   while(1);
 }
 
@@ -224,6 +187,12 @@ void writeInt(int num, int out)
   interrupt(33,0, str, out,0);
 }
 
+readSectors(char *buffer, int sector, int sectorCount)
+{
+  
+}
+
+
 /* ^^^^^^^^^^^^^^^^^^^^^^^^ */
 /* MAKE FUTURE UPDATES HERE */
 
@@ -233,7 +202,8 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
   switch(ax) {
    case 0:  printString(bx, cx); break;
     case 1: readString(bx); break;
-    /*case 2: case 3: case 4: case 5: */
+    case 2: readSectors(bx,cx,dx); break;
+    /*case 3: case 4: case 5: */
     /*  case 6: case 7: case 8: case 9: case 10: */
     /*  case 11: case 12: */
      case 13: writeInt(bx,cx); break;
