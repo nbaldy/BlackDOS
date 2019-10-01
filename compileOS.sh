70d472b9a320e701bdae8fab18124ce499d51699
nbaldy <baldy.nicole@gmail.com>
2019-10-01 11:45:19 -0400

    part 1

--- compileOS.sh
+++ compileOS.sh
@@ -3,5 +3,4 @@ dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 bcc -ansi -c -o kernel.o kernel.c
 ld86 -o kernel -d kernel.o kasm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
-
-dd if=msg of=floppya.img bs=512 count=1 seek=30 conv=notrunc
+dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc
